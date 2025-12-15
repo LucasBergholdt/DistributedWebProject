b0044e4c2de03eb0c20d1b14e333bb614f4c7998
Carl-Emil Dons <carlemildons@gmail.com>
2025-12-15 14:42:09 +0100

    Collective Details

--- artefact-1/app/__init__.py
+++ artefact-1/app/__init__.py
@@ -332,14 +332,21 @@ class Collective(db.Model):
         return Collective.query.order_by(Collective.id).all()
 
     @staticmethod
+    def get_by_id(id):
+        """Get collective by their ID"""
+        return Collective.query.filter_by(id=id).first()
+
+    @staticmethod
     def get_by_submitter(user_id):
         """Get all Collectives submitted by a specific user"""
         return Collective.query.filter_by(submitter_id=user_id).all()
     
+    @staticmethod
     def get_by_city(city):
         """Get all Collectives which cityname has given argument as prefix)"""
         return Collective.query.filter(Collective.city.startswith(city)).all()
     
+
     @classmethod
     def create_collective(cls, submitter_id, city, street, roomsize, price, description, image):
       """
@@ -435,11 +442,20 @@ def create_default_userbase():
   if not existing_provider:
      User.create_user("provider", "provider@gmail.com", "123")
 
+
+descr = """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris a finibus libero, at elementum urna. Sed dictum dapibus ornare. Maecenas egestas molestie vulputate. Donec maximus, ipsum a rhoncus eleifend, urna turpis volutpat mauris, id faucibus tellus turpis convallis tellus. Suspendisse a augue aliquet, dapibus risus et, condimentum turpis. Morbi finibus ultricies cursus. Nullam commodo felis eu facilisis lacinia. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+
+Vestibulum vestibulum neque eu lobortis malesuada. Pellentesque euismod erat mauris, non tempor lectus vulputate sit amet. Suspendisse eget nulla sed est lobortis imperdiet eu ut dui. Integer in semper ipsum, sed blandit sem. Maecenas consectetur vitae enim eu feugiat. Etiam non consectetur lorem. Sed non elit molestie, semper nulla vitae, sagittis eros. Suspendisse ante arcu, placerat vel ligula at, bibendum tincidunt tellus. Nam semper arcu neque, sit amet vestibulum felis commodo non. Nam in aliquet justo. Nulla auctor odio semper, eleifend massa et, volutpat purus. Suspendisse sit amet eros vel justo vehicula pharetra. Aenean tristique at ipsum id malesuada.
+"""
+
+
+
 # Debug Purposes
 def create_default_collectives_applications():
-  Collective.create_collective(2, "Odense C", "Vindegade", 50, 2569, "Just another great collective","1.jpg") #submitterID = 2. provider@gmail.com.
-  Collective.create_collective(2, "Odense M", "Bogense", 23, 5000, "Greetings! Welcome.", "2.jpg") #submitterID = 2. provider@gmail.com.
-  Collective.create_collective(2, "Odense M", "Stige", 35, 4000, "This is a great collective to live in! Please join me, Lasse and Lucas!", "3.jpg") #submitterID = 2. provider@gmail.com.
+  Collective.create_collective(2, "Odense C", "Vindegade", 50, 2569, descr,"1.jpg") #submitterID = 2. provider@gmail.com.
+  Collective.create_collective(2, "Odense M", "Bogense", 23, 5000, descr, "2.jpg") #submitterID = 2. provider@gmail.com.
+  Collective.create_collective(2, "Odense M", "Stige", 35, 4000, descr, "3.jpg") #submitterID = 2. provider@gmail.com.
 
   # Application.create_application(1, 1, "Jeg hedder Alice og vil gerne søge ind på kollektivet på Skovbogade.")
 
@@ -690,6 +706,10 @@ def overview():
       
     return render_template("overview.html", collective_entries=collective_entries)
 
+@app.route("/collective_details/<int:id>", methods=["GET","POST"])
+def collective_details(id):
+    entry = Collective.get_by_id(id)
+    return render_template("collective_details.html", entry=entry)
 
 @app.route("/profile",methods=["GET"])
 @login_required

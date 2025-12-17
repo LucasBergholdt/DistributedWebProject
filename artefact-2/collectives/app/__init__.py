f44c6962ef6462e05c03b21e1fb1aaab0d0adc1d
Carl-Emil Dons <carlemildons@gmail.com>
2025-12-17 21:33:24 +0100

    Upload/Download single images virker, men ikke rendering

--- artefact-2/collectives/app/__init__.py
+++ artefact-2/collectives/app/__init__.py
@@ -24,7 +24,7 @@ class Collective(db.Model):
     price = db.Column(db.Integer())
     
     description = db.Column(db.String(5000)) # todo: ændret til 5000, da debug strengen er for lang til 500. Hvorofr virker det så i monolith?
-    image = db.Column(db.String(5000))
+    image_name = db.Column(db.String(5000))
 
     # Overvej konsekvens af manglende "relationships?"
     # Mere kompleks?
@@ -72,7 +72,7 @@ class Collective(db.Model):
         return query.all()
 
     @classmethod
-    def create_collective(cls, submitter_id, city, street, roomsize, price, description, image):
+    def create_collective(cls, submitter_id, city, street, roomsize, price, description, image_name):
       """
       Create a new collective with the provided details.
 
@@ -90,7 +90,7 @@ class Collective(db.Model):
           roomsize = roomsize,
           price = price,
           description = description.strip(),
-          image = image.strip()
+          image_name = image_name.strip()
       )
 
       db.session.add(collective)
@@ -113,12 +113,8 @@ class Collective(db.Model):
             "roomsize": self.roomsize,
             "price": self.price,
             "description": self.description,
-            "image": self.image
+            "image_name": self.image_name
         }
-  
-  
-    
-
 
 # Debug Purposes ------------------
 descr = """
@@ -170,9 +166,9 @@ def post_collectives():
     price = data.get('price')
     description = data.get('description')
     roomsize = data.get('roomsize')
-    image = data.get('image')
+    image_name = data.get('image_name')
 
-    if not all([submitter_id, price, city, street, description, roomsize, image]): # works because None = False
+    if not all([submitter_id, price, city, street, description, roomsize, image_name]): # works because None = False
         return jsonify({"error": "Missing data"}), 400
 
     collective = Collective.create_collective(
@@ -182,10 +178,9 @@ def post_collectives():
         roomsize,
         price,
         description,
-        image
+        image_name
     )
     return jsonify(collective.to_dict()), 201
-
 
 @app.route("/collectives/<int:id>", methods=["GET"])
 def view_collective(id):
@@ -194,8 +189,19 @@ def view_collective(id):
         return jsonify(entry.to_dict()), 200
     else:
         return jsonify({"error": "Collective not found"}), 404
-    
 
+@app.route("/collectives/<int:id>", methods=["DELETE"])
+def collectives_delete(id):
+    """ 
+    Deletes a collective entry. 
+    """
+    entry = Collective.get_by_id(id)
+    if entry:
+        db.session.delete(entry)
+        db.session.commit()
+        return jsonify({"success": "Collective successfully deleted"}), 200
+    else:
+        return jsonify({"error": "Collective not found."}), 404
 
 
 
--- artefact-2/frontend/app/__init__.py
+++ artefact-2/frontend/app/__init__.py
@@ -144,7 +144,7 @@ def upload_image(file_storage):
         form_image: A werkzeug.datastructures.FileStorage object
 
     Returns:
-        str: The stored filename
+        str: The stored imagename
     """
 
     # define dictionary files, mapping a "file" to a 3-tuple.
@@ -162,7 +162,7 @@ def upload_image(file_storage):
     if response.status_code == 201:
       # return newly stored name
       data = response.json()
-      return data["filename"]
+      return data["image_name"]
 
 
 
@@ -379,7 +379,7 @@ def collectives_index():
     if response.ok:
       data = response.json()
       if params: 
-        flash(f"Filters: {filters.city.data}, {filters.price.data}, {filters.roomsize.data} applied", "success")  
+        flash(f"Filters: {filters.city.data}, {filters.price.data}, {filters.roomsize.data} applied", "success")
   
   # Or get all. 
   else: 
@@ -393,14 +393,23 @@ def collectives_index():
   response = requests.get(f"{PICTURES_API}/pictures", data=data)
 
   return render_template("collectives/index.html", collective_entries=data, form=filters)
-    
+
 #TODO: Crasher når vi går til kollektiv der ikke eksisterer.
 @app.route("/collectives/<int:id>", methods=["GET"])
 def collectives_view(id):
   response = requests.get(f"{COLLECTIVES_API}/collectives/{id}")
   if response.status_code == 200:
     entry = response.json()
-    return render_template("collectives/view.html", entry=entry)
+    picture_url = f"{PICTURES_API}/pictures/{entry['image_name']}"
+    # flash(f"The picture URL is {picture_url}")
+    #DEBUG
+    # response = requests.get(picture_url)
+    #if response.ok:
+    #  flash("DENNE EXECUTES.")
+    #else:
+    #  flash("Denne executes".)
+
+    return render_template("collectives/view.html", entry=entry, picture_url=picture_url)
   else:
     flash("Couldn't get collective", "error")
     return redirect(url_for("collectives_index"))
@@ -429,32 +438,28 @@ def provider_collectives():
 def collectives_create():
   form = CollectiveForm()
 
-  if request.method == 'POST':
-    if form.validate(): #TODO: Tror form.validate er nok alene.
-      
-      # ------------ Upload Image ---------------
-      file_storage = form.image.data
+  if form.validate_on_submit():
 
-      # Setup form data in dictionary to be sent with request
-      files = {
-      # value can be fetched by request.files["file"].
-        "file": (
-          file_storage.filename,
-          file_storage.stream,  
-          file_storage.mimetype #TODO not used right now
-        )
-      }
+    file_storage = form.image.data
 
-    # By sending data as "files", it is sent as a multipart/formdata API request.
-      response = requests.post(f"{PICTURES_API}/pictures", files = files)
-      if response.status_code == 201:
+    # make dictionary object with "file" pointing to 3 values. can be fetched by request.files["file"].
+    files = {
+      "file": (
+        file_storage.filename,
+        file_storage.stream,  
+        file_storage.mimetype #TODO not used right now
+      )
+    }
 
-        # return newly stored name
-        data = response.json()
-        new_filename = data["filename"]
-        flash("Image was uploaded! Name is" + new_filename)
-    
-      # TODO: Ikke lav collective hvis der går fejl i image.
+    # create image. #TODO ERROR HER!
+    response = requests.post(f"{PICTURES_API}/pictures", files = files) # By sending data as "files", it is sent as a multipart/formdata API request.
+    flash(f"Response code after uploading picture: {response.status_code}") #TODO debug
+    if response.status_code == 201:
+      # if success, create collective
+      data = response.json()
+      image_name = data["image_name"]
+      flash("Image was uploaded! Name is" + image_name)
+
       collective_data = {
         "submitter_id": session.get("user_id"), #TODO Lavet hurtigt af C-E, virker dette?
         "city": form.city.data,
@@ -462,19 +467,32 @@ def collectives_create():
         "roomsize": form.roomsize.data,
         "price": form.price.data,
         "description": form.description.data,
-        "image": new_filename
+        "image_name": image_name
       }
-      
-      # POST /collectives: Try to create collective
-      # #TODO Hvis failure: slet image entry.
+    
       response = requests.post(f"{COLLECTIVES_API}/collectives", json=collective_data)
-      flash("Response code:" + str(response.status_code))
-      if response.ok: # TODO: Status code er 500 lige nu?
+      flash(f"Response code after uploading collective: {response.status_code}")  #TODO debug
+      if response.ok:
         flash("Collective created!", "success")
         return redirect(url_for("provider_collectives"))
       else:
-        flash("Error creating collective", "error")
+        flash("Error creating collective. Removing previously uploaded image...", "error")  #TODO remove image
     else:
-      flash("Invalid input", "error")
+      flash("Image could not be uploaded.")
+  else:
+    flash("Invalid input", "error")
     
   return render_template("collectives/create.html", form=form)
+
+@app.route("/collectives/delete/<int:id>", methods=["POST"])
+def collectives_delete(id):
+    """ 
+    Deletes a collective entry. Method is POST because HTML cannot send DELETE requests. 
+    """
+    response = requests.delete(f"{COLLECTIVES_API}/collectives/{id}")
+    if response.ok:
+      #TODO: Remove image too.
+      flash(flash("Collective deleted.","success"))
+    else:
+      flash("Sorry, we couldn't find the collective that you wanted to delete.", 'warning')
+    return redirect(url_for('provider_collectives'))
